5a547d393f6887e6b2492ffb6a739f3510fa67ae
reza <reza1831amin@gmail.com>
2023-02-04 10:22:06 +0330

    initialize humanize to settings

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     # ! third party apps
     'jalali_date',
